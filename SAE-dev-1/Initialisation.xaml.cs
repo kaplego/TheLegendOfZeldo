e9e68fd9c312be4d17f9dce6ecd8b28373bd5643
kaplego <70806521+kaplego@users.noreply.github.com>
2024-01-08 14:46:16 +0100

    Fenêtre d'initialisation (début)

---
+++ SAE-dev-1/Initialisation.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SAE_dev_1
+{
+    /// <summary>
+    /// Logique d'interaction pour Initialisation.xaml
+    /// </summary>
+    public partial class Initialisation : Window
+    {
+        public Initialisation()
+        {
+            InitializeComponent();
+        }
+
+        public void Chargement(int valeur, string? nom = null)
+        {
+            chargement.Value = valeur;
+            if (nom != null)
+            {
+                nomChargement.Content = nom;
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
--- SAE-dev-1/MainWindow.xaml.cs
+++ SAE-dev-1/MainWindow.xaml.cs
@@ -21,16 +21,31 @@ namespace SAE_dev_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer minuteurJeu = new DispatcherTimer();
+        private ImageBrush imageCarte = new ImageBrush();
+        private ImageBrush imagePersonnage = new ImageBrush();
+        private ImageBrush imagePolice = new ImageBrush();
+        private ImageBrush imageObjets = new ImageBrush();
+
         int vitesseJ = 5;
         bool droite , gauche, bas, haut;
 
-
-        
-
-        private DispatcherTimer minuteurJeu = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Hide();
+
+            Initialisation fenetreInitialisation = new Initialisation();
+            fenetreInitialisation.Show();
+
+            fenetreInitialisation.Chargement(0, "Chargement des textures...");
+            imageCarte.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "ressources\\Overworld.png"));
+            imagePersonnage.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "ressources\\character.png"));
+            imagePolice.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "ressources\\font.png"));
+            imageObjets.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "ressources\\objects.png"));
+            fenetreInitialisation.Chargement(100);
+            
             minuteurJeu.Tick += MoteurDeJeu;
             minuteurJeu.Interval = TimeSpan.FromMilliseconds(16);
             minuteurJeu.Start();
